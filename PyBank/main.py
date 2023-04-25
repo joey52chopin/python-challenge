83e659196cc792afae446639b2134b21232b6108
Joey Zheng <joey52chopin@gmail.com>
2023-04-25 21:19:45 +0930

    uploading content

---
+++ PyBank/main.py
@@ -0,0 +1,3 @@
+import os
+import csv
+budget_csv=os.path.join("..","Resource","budget_data.csv")
---
+++ PyPoll/main.py
@@ -0,0 +1,3 @@
+import os
+import csv
+election_csv=os.path.join("..","Resource","election_data.csv")
